000be26a2a49da860136bc787870cfd0584d07a2
estesthebestes <esteshunter22@gmail.com>
2022-05-15 12:20:12 -0500

    found bug in the addEventListener, there were two cs in DOMContent --> DOMcContent

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-document.addEventListener('DOMcContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const hunter = document.querySelector('.hunter')
     const game = document.querySelector('.container')
     const ground = document.querySelector('.ground')
 
-    let hunterLeft = 200
+    let hunterLeft = 100
     let hunterBottom = 100
 
-    function startGame(){
+    function startGame() {
+        hunter.style.bottom = hunterBottom + 'px'
         hunter.style.left = hunterLeft + 'px'
     }
     startGame()
-});
+})
