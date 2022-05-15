621db4d8b5203f690622d0f110597fe69436c420
estesthebestes <esteshunter22@gmail.com>
2022-05-15 13:17:32 -0500

    added a way to generate new pipes and have them move to the left, however I need to be able to remove them after the pass through the game window on the left side, but it does not seem to be working that way

--- app.js
+++ app.js
@@ -14,12 +14,40 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     let gameTimer = setInterval(startGame, 20)
 
+    function jumpButton(e) {
+        if (e.keyCode === 32) {
+            jump()
+        }
+    }
+
     function jump() {
-    
         if (hunterBottom < 430) hunterBottom += 50
         hunter.style.bottom = hunterBottom + 'px'
         console.log(hunterBottom)
     }
-    document.addEventListener('keydown', jump)
+    document.addEventListener('keyup', jumpButton)
     
+    function makePipe() {
+        let pipeLeft = 460
+        let randomHeight = Math.random() * 100
+        let pipeBottom = randomHeight
+        const pipe = document.createElement('div')
+        pipe.classList.add('pipe')
+        game.appendChild(pipe)
+        pipe.style.left = pipeLeft + 'px'
+        pipe.style.bottom = pipeBottom + 'px'
+
+        function movePipe() {
+            pipeLeft -= 1.5
+            pipe.style.left = pipeLeft + 'px'
+
+            if (pipeLeft === -40) {
+                clearInterval(gameTimer)
+                game.removeChild(pipe)
+            }
+        }
+        let gameTimer = setInterval(movePipe, 20)
+        setTimeout(makePipe, 2000)
+    }
+    makePipe()
 })
